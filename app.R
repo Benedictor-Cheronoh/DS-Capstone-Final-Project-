d3b6b8309280a8b8ea0254752c4adbcf5979be08
Benedictor-Cheronoh <120311362+Benedictor-Cheronoh@users.noreply.github.com>
2023-09-19 09:16:46 +0300

    app.R

--- app.R
+++ app.R
@@ -5,3 +5,6 @@ source("server.R")
 
 # Create shiny application ------------------------------------------
 shinyApp(ui = ui, server = server)
+
+##The user interface (ui) object controls the layout and appearance of your app. The server function contains the instructions that your computer needs to build your app. 
+Finally the shinyApp function creates Shiny app objects from an explicit UI/server pair.
